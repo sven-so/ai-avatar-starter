c562752b24f02359e187b63ea554e48b07f91ee0
sven-so <svenso@web.de>
2023-01-17 23:39:25 +0100

    step 4: smaller UI fixes

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,19 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const exampleArray = [
+    "Closeup face portrait of svenso wearing crown of flowers, smooth soft skin, big dreamy eyes, intricate colored hair, symmetrical, anime wide eyes, soft lighting, detailed face, by makoto shinkai, stanley artgerm lau, wlop, rossdraws, concept art, digital painting, looking into camera",
+    "Great lighting, portrait of svenso, in a solar punk setting highly detailed background",
+    "Portrait photo of svenso, highly detailed, by karsh",
+    "svenso intricate character portrait, intricate, beautiful, 8k resolution, dynamic lighting, hyperdetailed, quality 3D rendered, volumetric lighting, pixar, detailed background",
+    "svenso as a character from disneys frozen, au naturel, PS2, PS1, hyper detailed, digital art, trending in artstation, cinematic lighting, studio quality, smooth render, unreal engine 5 rendered, octane rendered",
+    "portrait of svenso as a 3d animated dwarf from lord of the rings, Ultra detailed, concept design, highly detailed, pincushion lens effect, 3d render",
+    "portrait of svenso celebrating st.patricks day, Ultra detailed, concept design, highly detailed, pincushion lens effect, 3d render"
+  ];
   const maxRetries = 20;
   const [input, setInput] = useState('');
   const [img, setImg] = useState('');
+  const [example, setExample] = useState('');
   const [retry, setRetry] = useState(0);
   const [retryCount, setRetryCount] = useState(maxRetries);
   const [isGenerating, setIsGenerating] = useState(false);
@@ -14,6 +24,9 @@ const Home = () => {
   const onChange = (event) => {
     setInput(event.target.value);
   };
+  const showHelp = (event) => {
+      setExample("Example Text: "+exampleArray[Math.floor(Math.random() * 6)]);
+  }
   const generateAction = async () => {
     console.log('Generating...');
     if (isGenerating && retry === 0) return;
@@ -29,13 +42,15 @@ const Home = () => {
 
       setRetry(0);
     }
+    const finalInput = input.replace('svenso', 'svsoenso man');
+    console.log(finalInput);
     // Add the fetch request
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'image/jpeg',
       },
-      body: JSON.stringify({ input }),
+      body: JSON.stringify({ input: finalInput }),
     });
 
     const data = await response.json();
@@ -92,10 +107,12 @@ const Home = () => {
             <h1>Make funny pictures of me</h1>
           </div>
           <div className="header-subtitle">
-            <h2>just write how you would like to see me and make sure to use "svsoenso man" in the prompt</h2>
+            <h2>just write into the first text field how you would like to see me and make sure to use "svenso" in the prompt</h2>
+            {example && (<h2 className='example'>{example}</h2>)}
           </div>
           {/* Add prompt container here */}
           <div className="prompt-container">
+            <a className='help-button' onClick={showHelp}>?</a>
             <input className="prompt-box" value={input} onChange={onChange} />
             <div className="prompt-buttons">
               <a className={
